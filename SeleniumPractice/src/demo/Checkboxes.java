f1f15f4eb54522dbc0a342ca363274bf96f9a78a
hp <hp@hp-PC>
2019-09-19 16:43:14 -0400

    chnges in checkbox

--- SeleniumPractice/src/demo/Checkboxes.java
+++ SeleniumPractice/src/demo/Checkboxes.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Checkboxes {
-
+ // comment 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
